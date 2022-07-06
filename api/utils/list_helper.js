7d320ca0268d210061dfe1d9dd99a1105cff265c
flportilla <flportilla@gmail.com>
2022-07-06 11:43:30 -0500

    Add simple test to check Jest

--- api/utils/list_helper.js
+++ api/utils/list_helper.js
@@ -8,6 +8,30 @@ function totalLikes(blogs) {
   }, 0)
 }
 
+function mostLiked(blogs) {
+  if (blogs.length === 0) return 0
+
+  const maxLikes = blogs.reduce((prev, curr) => {
+    return Math.max(prev, curr.likes);
+  }, 0)
+
+  const mostLikedBlog = blogs.filter(blog => blog.likes === maxLikes)
+
+  const result = {
+    title: mostLikedBlog[0].title,
+    author: mostLikedBlog[0].author,
+    likes: mostLikedBlog[0].likes
+  }
+
+  return result
+}
+
+function mostBlogs(blogs) {
+
+}
+
 module.exports = {
   totalLikes,
+  mostLiked,
+  mostBlogs
 }
